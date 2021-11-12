7d128157f2a14dfbaba225cfacc88e2166ed61de
ceilingwin <thangftn99@gmail.com>
2021-11-13 02:38:42 +0700

    add tile map project

---
+++ tile_map/MapObject.tsx
@@ -0,0 +1,242 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="MapObject" tilewidth="818" tileheight="699" tilecount="75" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="200" height="107" source="textures/80-100.png"/>
+ </tile>
+ <tile id="1">
+  <image width="417" height="282" source="textures/223-100.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="128" source="textures/242-100.png"/>
+ </tile>
+ <tile id="3">
+  <image width="102" height="84" source="textures/260-100.png"/>
+ </tile>
+ <tile id="4">
+  <image width="97" height="81" source="textures/261-100.png"/>
+ </tile>
+ <tile id="5">
+  <image width="281" height="176" source="textures/263-100.png"/>
+ </tile>
+ <tile id="6">
+  <image width="176" height="134" source="textures/264-67.png"/>
+ </tile>
+ <tile id="7">
+  <image width="139" height="139" source="textures/265-71.png"/>
+ </tile>
+ <tile id="8">
+  <image width="197" height="196" source="textures/265-100.png"/>
+ </tile>
+ <tile id="9">
+  <image width="141" height="143" source="textures/266-100.png"/>
+ </tile>
+ <tile id="10">
+  <image width="148" height="143" source="textures/267-70.png"/>
+ </tile>
+ <tile id="11">
+  <image width="433" height="424" source="textures/268-100.png"/>
+ </tile>
+ <tile id="12">
+  <image width="696" height="500" source="textures/269-100.png"/>
+ </tile>
+ <tile id="13">
+  <image width="433" height="424" source="textures/270-100.png"/>
+ </tile>
+ <tile id="14">
+  <image width="433" height="424" source="textures/272-100.png"/>
+ </tile>
+ <tile id="15">
+  <image width="256" height="128" source="textures/273-100.png"/>
+ </tile>
+ <tile id="16">
+  <image width="212" height="123" source="textures/275-26.png"/>
+ </tile>
+ <tile id="17">
+  <image width="441" height="257" source="textures/275-54.png"/>
+ </tile>
+ <tile id="18">
+  <image width="605" height="442" source="textures/276-81.png"/>
+ </tile>
+ <tile id="19">
+  <image width="433" height="252" source="textures/277-53.png"/>
+ </tile>
+ <tile id="20">
+  <image width="818" height="476" source="textures/277-100.png"/>
+ </tile>
+ <tile id="21">
+  <image width="273" height="261" source="textures/278-43.png"/>
+ </tile>
+ <tile id="22">
+  <image width="489" height="468" source="textures/278-77.png"/>
+ </tile>
+ <tile id="23">
+  <image width="240" height="170" source="textures/279-43.png"/>
+ </tile>
+ <tile id="24">
+  <image width="284" height="238" source="textures/280-50.png"/>
+ </tile>
+ <tile id="25">
+  <image width="569" height="477" source="textures/280-100.png"/>
+ </tile>
+ <tile id="26">
+  <image width="252" height="225" source="textures/281-100.png"/>
+ </tile>
+ <tile id="27">
+  <image width="190" height="185" source="textures/282-72.png"/>
+ </tile>
+ <tile id="28">
+  <image width="193" height="188" source="textures/282-73.png"/>
+ </tile>
+ <tile id="29">
+  <image width="118" height="102" source="textures/283-49.png"/>
+ </tile>
+ <tile id="30">
+  <image width="142" height="123" source="textures/283-59.png"/>
+ </tile>
+ <tile id="31">
+  <image width="136" height="120" source="textures/284-38.png"/>
+ </tile>
+ <tile id="32">
+  <image width="234" height="206" source="textures/284-65.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="29.5" y="126">
+    <polygon points="0,0 -1,-25.5 13.5,-43 41.5,-63.5 54.5,-83.5 77,-95.5 156,-71.5 176,-32.5 139.5,39 94,43 74,50"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <image width="166" height="156" source="textures/285-69.png"/>
+ </tile>
+ <tile id="34">
+  <image width="242" height="227" source="textures/285-100.png"/>
+ </tile>
+ <tile id="35">
+  <image width="196" height="191" source="textures/286-79.png"/>
+ </tile>
+ <tile id="36">
+  <image width="173" height="142" source="textures/287-50.png"/>
+ </tile>
+ <tile id="37">
+  <image width="346" height="284" source="textures/287-100.png"/>
+ </tile>
+ <tile id="38">
+  <image width="363" height="400" source="textures/288-100.png"/>
+ </tile>
+ <tile id="39">
+  <image width="370" height="342" source="textures/290-55.png"/>
+ </tile>
+ <tile id="40">
+  <image width="674" height="623" source="textures/290-100.png"/>
+ </tile>
+ <tile id="41">
+  <image width="600" height="600" source="textures/293-100.png"/>
+ </tile>
+ <tile id="42">
+  <image width="627" height="595" source="textures/296-100.png"/>
+ </tile>
+ <tile id="43">
+  <image width="256" height="128" source="textures/297-100.png"/>
+ </tile>
+ <tile id="44">
+  <image width="617" height="595" source="textures/298-100.png"/>
+ </tile>
+ <tile id="45">
+  <image width="396" height="381" source="textures/303-66.png"/>
+ </tile>
+ <tile id="46">
+  <image width="600" height="578" source="textures/303-100.png"/>
+ </tile>
+ <tile id="47">
+  <image width="360" height="346" source="textures/304-60.png"/>
+ </tile>
+ <tile id="48">
+  <image width="600" height="578" source="textures/304-100.png"/>
+ </tile>
+ <tile id="49">
+  <image width="650" height="651" source="textures/316-100.png"/>
+ </tile>
+ <tile id="50">
+  <image width="230" height="208" source="textures/317-100.png"/>
+ </tile>
+ <tile id="51">
+  <image width="339" height="352" source="textures/318-100.png"/>
+ </tile>
+ <tile id="52">
+  <image width="173" height="169" source="textures/356-73.png"/>
+ </tile>
+ <tile id="53">
+  <image width="256" height="128" source="textures/378-100.png"/>
+ </tile>
+ <tile id="54">
+  <image width="422" height="421" source="textures/397-100.png"/>
+ </tile>
+ <tile id="55">
+  <image width="600" height="557" source="textures/398-100.png"/>
+ </tile>
+ <tile id="56">
+  <image width="600" height="599" source="textures/399-100.png"/>
+ </tile>
+ <tile id="57">
+  <image width="285" height="263" source="textures/401-100.png"/>
+ </tile>
+ <tile id="58">
+  <image width="246" height="241" source="textures/402-57.png"/>
+ </tile>
+ <tile id="59">
+  <image width="238" height="238" source="textures/404-76.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="body" x="0" y="0" width="214" height="214"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <image width="223" height="201" source="textures/411-100.png"/>
+ </tile>
+ <tile id="61">
+  <image width="212" height="212" source="textures/415-53.png"/>
+ </tile>
+ <tile id="62">
+  <image width="300" height="200" source="textures/418-100.png"/>
+ </tile>
+ <tile id="63">
+  <image width="300" height="283" source="textures/419-100.png"/>
+ </tile>
+ <tile id="64">
+  <image width="455" height="455" source="textures/420-91.png"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" name="body" x="30" y="30" width="400" height="400">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="65">
+  <image width="200" height="187" source="textures/421-100.png"/>
+ </tile>
+ <tile id="66">
+  <image width="200" height="149" source="textures/422-100.png"/>
+ </tile>
+ <tile id="67">
+  <image width="388" height="258" source="textures/425-97.png"/>
+ </tile>
+ <tile id="68">
+  <image width="256" height="128" source="textures/429-100.png"/>
+ </tile>
+ <tile id="69">
+  <image width="200" height="106" source="textures/431-100.png"/>
+ </tile>
+ <tile id="70">
+  <image width="600" height="600" source="textures/436-100.png"/>
+ </tile>
+ <tile id="71">
+  <image width="300" height="294" source="textures/437-100.png"/>
+ </tile>
+ <tile id="72">
+  <image width="140" height="79" source="textures/441-70.png"/>
+ </tile>
+ <tile id="73">
+  <image width="395" height="357" source="textures/466-79.png"/>
+ </tile>
+ <tile id="74">
+  <image width="700" height="699" source="textures/483-100.png"/>
+ </tile>
+</tileset>
