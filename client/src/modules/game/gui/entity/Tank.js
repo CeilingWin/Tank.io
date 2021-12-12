d5f496dd3a45d6a0a0956470167be0b847b38766
ceilingwin <thangftn99@gmail.com>
2021-12-12 09:24:16 +0700

    res tank

--- client/src/modules/game/gui/entity/Tank.js
+++ client/src/modules/game/gui/entity/Tank.js
@@ -1,7 +1,18 @@
 var Tank = cc.Node.extend({
     ctor: function(){
         this._super();
-        let tank = new cc.Sprite("res/map/textures/object/80-100.png");
-        this.addChild(tank);
+        this._init();
     },
+    
+    _init: function () {
+        // body
+        let body = new cc.Sprite("res/tank/body.png");
+        this.addChild(body);
+        this.body = body;
+        //cannon
+        let cannon = new cc.Sprite("res/tank/cannon1.png");
+        cannon.anchorX = 0.4;
+        this.addChild(cannon);
+        this.cannon = cannon;
+    }
 })
